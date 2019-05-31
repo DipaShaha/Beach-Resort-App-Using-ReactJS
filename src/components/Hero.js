24ef7ecd586336dd16040545fe269db27be5647f
DipaShaha <dipashaha92@gmail.com>
2019-06-01 02:59:50 +0600

    completed hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,1 +1,14 @@
-Hero.js
+import React from 'react';
+
+
+export default const Hero=(children,hero)=>{
+	return(
+		<header className={hero}>
+			{children}
+		</header>
+
+	);
+}
+Hero.defaultProps={
+	hero:"defaultHero"
+}

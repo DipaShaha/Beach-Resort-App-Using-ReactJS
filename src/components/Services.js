c306281d0dad5eb91c88a74bd337a1d2a268009e
DipaShaha <dipashaha92@gmail.com>
2019-06-01 16:34:40 +0600

    Added  Services Section

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-
+import {FaCocktail,FaHiking,FaShuttleVan,FaBeer} from 'react-icons/fa';
+import Title from './Title';
 
 class Services extends Component {
+    state={
+        services:[
+            {
+                icon:<FaCocktail/>,
+                title:"Free Cocktail",
+                info:"Sometimes known, is dummy text used in laying out print, graphic or web designs."
+            },
+             {
+                icon:<FaHiking/>,
+                title:"Free Hiking",
+                info:"Sometimes known, is dummy text used in laying out print, graphic or web designs."
+            },
+            {
+                icon:<FaShuttleVan/>,
+                title:"Free Shuttle",
+                info:"Sometimes known, is dummy text used in laying out print, graphic or web designs."
+            },
+            {
+                icon:<FaBeer/>,
+                title:"Strongest Beer",
+                info:"Sometimes known, is dummy text used in laying out print, graphic or web designs."
+            }
+
+        ]
+    }
+
 
   render() {
-    return (
+    return(
+        <section className="services">
+            <Title title="services"/>
+            <div className="services-center">
+                {this.state.services.map((item,index)=>{
+                        return(
+                        <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                        )
+                    }
+                )}  
+            </div>
+        </section>
+
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import Services from '../components/Services';
 import {Link} from 'react-router-dom';
 const Home=()=>{
     return (
+    	<React.Fragment>
         	<Hero >
 		        <Banner title="luxurious rooms" subtile="delux rooms starting at $333">
 		        	<Link to="/rooms" className="btn-primary">
@@ -11,6 +13,9 @@ const Home=()=>{
 		        	</Link>
 		        </Banner>
         	</Hero>
+
+        	<Services/>
+        </React.Fragment>
     );
 }
 
